0e6ae00102a10f5656b5dd877fa4bbc964714456
majidn92 <majidn92@gmail.com>
2022-08-24 18:30:26 +0430

    2

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,7 +4,7 @@
 @endsection
 
 @section('content')
-<iframe src="{{ url('1.pdf') }}" width="250" height="250">
+<iframe src="{{ url('1.pdf') }}" width="250" height="250"> 
 @endsection
 
 @section('script')
